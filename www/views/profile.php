8ca577499f7b9cb6020334f1efafcba6dd4ec2a5
Volodymyr Nazaryshyn <volodimirnazarisin@gmail.com>
2022-12-07 13:47:51 +0200

    Profile homework

--- www/views/profile.php
+++ www/views/profile.php
@@ -1,5 +1,26 @@
-<h1>Profile</h1>
+<h1>My Profile</h1>
 
-<pre><?php print_r( $_PROF_DATA) ?>
+<img class='profile-avatar' src='/avatars/<?= empty($_PROF_DATA['avatar']) ? 'no-avatar.png' : $_PROF_DATA['avatar'] ?>' alt="avatar">
 
-Д.З. Оформить вид личного кабинета
+<h3>Welcome,</h4>
+
+<h2 class='profile-name'><?= $_PROF_DATA['name'] ?></h3>
+
+<table class='profile-table' >
+    <tr>
+        <td>Login</td>
+        <td><input class='profile-input' value='<?= $_PROF_DATA['login'] ?>' /></td>
+    </tr>
+    <tr>
+        <td>Name</td>
+        <td><input class='profile-input' value='<?= $_PROF_DATA['name'] ?>' /></td>
+    </tr>
+    <tr>
+        <td>E-mail</td>
+        <td><input class='profile-input' type="email" value='<?= $_PROF_DATA['email'] ?>' /></td>
+    </tr>
+</table>
+
+<button class='update-profile-btn'>Update Profile</button>
+
+<!-- + Оформить вид личного кабинета -->
