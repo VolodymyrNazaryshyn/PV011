d58baeb7706e8daf2265790bc40061e52a63b041
Volodymyr Nazaryshyn <volodimirnazarisin@gmail.com>
2022-12-16 00:43:53 +0200

    Hide form from everyone except admin & show msg HW

--- www/views/components/shop.php
+++ www/views/components/shop.php
@@ -15,13 +15,13 @@
         <img src="/images/<?= $product['image'] ?>" />
     </div>
     <h4><?= $product['name'] ?></h4>
-    <h5><?= $product['descr'] ?></h5>
+    <h5><?= empty($product['descr']) ? 'Нет описания' : $product['descr'] ?></h5>
     <b><?= $product['price'] ?></b>
     <?php if( ! empty( $product['discount'] ) ) : ?>
         (<i><?= $product['discount'] ?></i>)
     <?php endif ?>
     <div class="rating-area">
-        <span>(<?= $product['rating'] ?>)</span>
+        <span>(<?= empty($product['rating']) ? '0' : $product['rating'] ?>)</span>
         <input type="radio" id="star-5<?=$product['id']?>" name="rating<?=$product['id']?>" value="5" <?= ($product['rating'] > 4) ? 'checked' : '' ?> />
         <label for="star-5<?=$product['id']?>" title="Grade «5»"></label>
         <input type="radio" id="star-4<?=$product['id']?>" name="rating<?=$product['id']?>" value="4" <?= ($product['rating'] > 3 && $product['rating'] <= 4) ? 'checked' : '' ?> />
@@ -30,7 +30,7 @@
         <label for="star-3<?=$product['id']?>" title="Grade «3»"></label>
         <input type="radio" id="star-2<?=$product['id']?>" name="rating<?=$product['id']?>" value="2" <?= ($product['rating'] > 1 && $product['rating'] <= 2) ? 'checked' : '' ?> />
         <label for="star-2<?=$product['id']?>" title="Grade «2»"></label>
-        <input type="radio" id="star-1<?=$product['id']?>" name="rating<?=$product['id']?>" value="1" <?= ($product['rating'] <= 1) ? 'checked' : '' ?> />
+        <input type="radio" id="star-1<?=$product['id']?>" name="rating<?=$product['id']?>" value="1" <?= ($product['rating'] > 0 && $product['rating'] <= 1) ? 'checked' : '' ?> />
         <label for="star-1<?=$product['id']?>" title="Grade «1»"></label>
     </div>
     <u>Since <?= date( "d.m.y", strtotime( $product['add_dt'] ) ) ?></u>
@@ -63,26 +63,22 @@
     <?php endif ?>
 </div>
 
-
-
-<form method="post" enctype="multipart/form-data">
-    <input type="text"   name="name"     placeholder="Название" /><br/>
-    <textarea            name="descr"    placeholder="Описание" ></textarea><br/>
-    <input type="number" name="price"    placeholder="Цена" /><br/>
-    <input type="number" name="discount" placeholder="Скидка" /><br/>
-    <input type="file"   name="image"  /><br/>
-    <button>Добавить</button>
-</form>
+<?php if( ! empty($_CONTEXT['auth_user']) && $_CONTEXT['auth_user']['id'] == 'd466150e-6c4d-11ed-a832-291d18215c69' ) : ?>
+    <form method="post" enctype="multipart/form-data">
+        <input type="text"   name="name"     placeholder="Название" /><br/>
+        <textarea            name="descr"    placeholder="Описание" ></textarea><br/>
+        <input type="number" name="price"    placeholder="Цена" /><br/>
+        <input type="number" name="discount" placeholder="Скидка" /><br/>
+        <input type="file"   name="image"  /><br/>
+        <button>Добавить</button>
+    </form>
+<?php endif ?>
 
 <?= $view_data[ 'add_error' ] ?? ''  ?>
 
 <?php
 
 /*
-Д.З. Создать контроллер shop_controller, реализовать прием данных формы
-добавления товара, обработать сохранить файл-картинку, подготовить
-(и выполнить) запрос на добавление данных в БД.
-
 CREATE TABLE Products (
     `id`        CHAR(36)       NOT NULL   PRIMARY KEY     COMMENT 'UUID',
     `id_grp`    CHAR(36)           NULL                   COMMENT 'Group ID',
